2f6e292a36718ec4bffc7527905d088f5ce97d77
vladimir <adptxaoca@gmail.com>
2020-04-26 22:33:50 +0700

    Added zap logger

--- cmd/part_handler/main.go
+++ cmd/part_handler/main.go
@@ -3,25 +3,32 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"os"
 
 	"part_handler/internal/app/config"
+	"part_handler/internal/pkg/logger"
 	"part_handler/internal/pkg/server"
 )
 
 var (
 	configPath = flag.String("config", "configs", "config file path")
+	logPath    = flag.String("log", "logs/parts.log", "log file path")
 )
 
 func main() {
 	flag.Parse()
 
+	log := logger.InitLogger(*logPath)
+	defer func() { _ = log.Sync() }()
+
 	conf, err := config.AppConfiguration(*configPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
+		os.Exit(1)
 	}
 
-	if err := server.Run(context.Background(), conf); err != nil {
-		log.Fatal(err)
+	if err := server.Run(context.Background(), log, conf); err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1)
 	}
 }
